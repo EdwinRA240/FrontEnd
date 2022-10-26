36adb9d2de52275edae4dcfdafb1e1db52a5269c
Edwin RA <edwinra240@gmail.com>
2022-10-25 21:33:15 -0500

    Fix

---
+++ 5-components/src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./style.css";
+
+function Card() {
+    
+    return (
+        <div> className="card"
+            <div> className="card-header"><h1>Titulo</h1></div>
+            <div> className="card-header"><p>Titulo</p></div>
+        </div>     
+    )
+}
+
+export default Card;
--- 5-components/src/components/Description.js
+++ 5-components/src/components/Description.js
@@ -1,7 +1,14 @@
+import styled from 'styled-components'
+import React from 'react';
+
 function Description() {
+    const Parrafo = styled.p`
+    font-size: 20px
+    font-weight: 700`
+
     return (
         <div>
-            <p>
+            <p className={Parrafo}>
                 qwertyuiop
             </p>
         </div>
