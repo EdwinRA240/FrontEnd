7a19cbb2ab08d628754148228030835834779394
Edwin RA <edwinra240@gmail.com>
2022-10-26 21:11:17 -0500

    new data

--- 3-create-react-app/src/App.js
+++ 3-create-react-app/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from 'components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenido a mi Linkedin
-        </p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/edwinemmanuelramirezaguilar/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go Linkedin
-        </a>
-      </header>
+    <>
+    <div>
+      <span>Components App!</span>
     </div>
+    <NavBar></NavBar>
+    </>
   );
 }
 
