3da2490da2c73e987d10b23412b0a5c0e46a8ba0
Edwin RA <edwinra240@gmail.com>
2022-11-02 21:10:23 -0600

    Feat: se añade uso de MUI para interfaz

--- 7-rick-and-morty-api/src/App.js
+++ 7-rick-and-morty-api/src/App.js
@@ -1,22 +1,36 @@
+import React from 'react';
 import {useState} from 'react';
 import Form from './components/Form';
 import ShowCharacter from './components/character/ShowCharacter';
 import './App.css';
 
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Container from '@mui/material/Container';
+import LockResetIcon from '@mui/icons-material/LockReset';
 
 function App() {
 
   const [name, setName] = useState("");
 
   return (
-    <div>
-      <h1>Rick&Morty API</h1>
-      <Form setName={setName}/>
-      <button>
-        Reset
-      </button>
-      <ShowCharacter name={name}/>
-    </div>
+    <>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography variant="h2">Rick&Morty API</Typography>
+        <Form setName={setName}/>
+
+        <IconButton color="primary" aria-label="Reset" component="label" onClick={()=>setName('')}>
+          {/* <LockResetIcon /> */}
+        </IconButton>
+
+        {/* <button onClick={()=>setName('')}>
+          Reset
+        </button> */}
+        <ShowCharacter name={name} margin="normal"/>
+      </Container>
+    </>
   );
 }
 
